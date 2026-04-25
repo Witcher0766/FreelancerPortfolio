1af7f4ef175c11209575cfbd64728ab962d1cb82
Witcher0766 <kumarvishwajeet076@gmail.com>
2026-04-25 21:28:05 +0530

    changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -91,9 +91,11 @@ const Profile = () => {
             <Typewriter
               options={{
                 strings: [
-                  "Full Stack Developer",
-                  "Software Development Engineer",
-                  "Android Developer"
+                  "Full Stack Developer 💻",
+                  "Android App Developer 📱",
+                  "Building Scalable Web Apps",
+                  "Code. Debug. Repeat.",
+                  "From UI to Backend ⚙️"
                 ],
                 autoStart: true,
                 loop: true,
